a087c4a2b71a63f5d0e662aea36aa90b92ff699d
Oswaldo Nickel <oswaldo.nickel@gmail.com>
2020-06-26 16:06:41 +0200

    verbesserung search

--- wpsearchdir.sh
+++ wpsearchdir.sh
@@ -5,7 +5,9 @@ dirs=($(ls -lh -d */ | awk '{print $9}'))
 #check for wpsites and add them to array
 for site in "${dirs[@]}"; do
     if [ -d "$site/wp-content/" ]; then
+        echo "Found $site"
         sites+=("$site")
+        let i++
     fi
 done
-echo "${sites[@]}"
+echo "$i"
