a98c0ad5a686b1a3367d80af8a9e3a5c5de4403e
Oswaldo Nickel <oswaldo.nickel@gmail.com>
2020-07-01 15:59:44 +0200

    add -w

--- wpupdate.sh
+++ wpupdate.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 dir=./
-declare -a sites
-
+wp="wp"         #where is wp-cli 
 while [ $# -gt 0 ];do
     case $1 in
         -d)
@@ -10,43 +9,54 @@ while [ $# -gt 0 ];do
             dir=$1
             ;;
         -h)
-            echo "wpupdate.sh [-d TARGET DIR]"
+            echo "wpupdate.sh [-d targetDIR][-w path/to/wp]"
             exit
+            ;;
+        -w)
+            shift
+            wp=$1
             ;;
     esac
     #next argument -> e.g. $2 becomes $1, $3 becomes $2...
     shift
 done
+#only WP-Sites are to be processed
 for site in $(ls -d $dir*/); do
     if [ -d "$site/wp-content/" ]; then
         sites+=("$site")
     fi
 done
+
+
           
 for site in "${sites[@]}"; do
-    echo -e "------------\n$site\n--------------"
-    cd $site
-    wp core check-update
+    #only names
+    site=${site##"$dir"}
+    site=${site%/}
+    echo -e "================\n\t\t$site\n================"
+    cd $site  &>/dev/null
+    echo -e "---------------\nCheck Core Update\n---------------"
+    $wp core check-update
     sleep 1 
     echo -e "\nProceed? [y/n]"
     read answer
     echo -e "\n--------------"
     if [ "$answer" = "y" ]; then
-        wp core update
+        $wp core update
     else
         echo "Nothin done"
     fi
-    sleep 1
-    wp plugin list
+    echo -e "---------------\nCheck Plugins\n---------------"
+    $wp plugin list
     sleep 1 
     echo -e "\nProceed? [y/n]"
     read answer
     echo -e "\n--------------"
     if [ "$answer" = "y" ]; then
-        wp plugin update --all
+        $wp plugin update --all
     else
         echo "Nothin done"
     fi
-    cd -
+    cd -  &>/dev/null
 done
 
