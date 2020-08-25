12f0323216275a6f957133359e8e7e490a5752e2
Oswaldo Nickel <oswaldo.nickel@gmail.com>
2020-08-25 16:20:08 +0200

    fix

---
+++ wprestore.sh
@@ -0,0 +1,96 @@
+#!/bin/env bash
+
+verbose=0       #show files
+lookup_dir=../../backup/wp/
+wp="wp"         #where is wp-cli 
+cms="wp"        #specific for db dump
+restore=0       #restoree site from file 
+while [ $# -gt 0 ];do
+    case $1 in
+        -v)
+            verbose=1
+            ;;
+        -d)
+            shift
+            dir=$1
+            ;;
+        -h)
+            echo "wpbackup.sh [-b][-r][-d targetDIR][-w path/to/wp]"
+            exit
+            ;;
+        -w)
+            shift
+            wp=$1
+            ;;
+    esac
+    #next argument -> e.g. $2 becomes $1, $3 becomes $2...
+    shift
+done
+#only WP-Sites are to be processed
+for site in $(ls -d $dir*/); do
+    if [ -d "$site/wp-content/" ]; then
+        seite=${site##"$dir"}
+        echo "Found $seite"
+        echo "Should it be processed? [y/n] "
+    cd $site  &>/dev/null
+        read answer
+        echo -e "\n--------------"
+        if [ "$answer" = "y" ]; then
+            echo "if name $site is wrong type in a new one"
+            read name
+            # when the name is corrected, put it inarray
+            # if not, put the original one with path in the same array
+            if [ ! -z "$name" ]; then
+                names+=("$name")
+            else
+                name=${site##"$dir"}
+                name=${name%/}
+                names+=("$name")
+            fi
+            sites+=("$site") #this array must contain the path for every site 
+                             # -> "cd $site"
+        fi
+    fi
+done
+
+root="${dir}backups"
+#backups folders for every site
+function folders () {
+    if [ ! -d "$1" ]; then
+        mkdir $1
+    fi
+}
+
+folders $root
+datum=$(date)          
+i=0
+for site in "${sites[@]}"; do
+    cd $site  &>/dev/null
+    #only names
+      echo -e "================================\n\t${names[$i]}\n================================"
+    # is wp-site working?
+    error=$($wp core check-update ) #the result of command -> 0 ok, 1 error. string goes to variable
+    #echo $?
+    if [ ! -z "$error" ]; then
+     #   echo "$error"
+        echo "Everything OK"
+    else
+        echo "$error" 
+        continue
+   fi
+   sleep 1
+    echo -e "---------------\nDumping Database\n---------------"
+    $wp db export "${names[$i]}-${datum}.sql" --allow-root
+    sleep 1
+    echo -e "---------------\nBacking up files\n---------------"
+    folders $root/${names[$i]}
+    if [ "$verbose" -eq 1 ]; then
+        tar cvzf "$root/${names[$i]}/${names[$i]}-${datum}.tar.gz" . 
+    else
+        tar czf "$root/${names[$i]}/${names[$i]}-${datum}.tar.gz" . 
+    fi
+   rm "${names[$i]}-${datum}.sql"    #CLEAN UP
+    cd -  &>/dev/null
+    ((i++))
+done
+
