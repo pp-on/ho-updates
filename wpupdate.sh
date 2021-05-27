80ad5380386c1cd36ea3f9e27a6bb09d5e96e879
Oswaldo Nickel <oswaldo.nickel@gmail.com>
2021-05-27 13:26:46 +0200

    fix: rewrite git algorhythm

--- wpupdate.sh
+++ wpupdate.sh
@@ -29,7 +29,7 @@ for site in $(ls -d $dir*/); do
     if [ -d "$site/wp-content/" ]; then
         seite=${site##"$dir"}
         echo "Found $seite"
-        echo "Should it be processed? [y/n] "
+        echo "Should it be processed? [y] "
         read answer
         echo -e "\n--------------"
         if [ "$answer" = "y" ]; then
@@ -42,7 +42,7 @@ function update_core () { #update wordpress, only when there is a new version
     succes=$($wp core check-update 2>/dev/null| grep Success) #0 -> ok ,1 -> err in bash
     #echo $?
     if [ -z "$succes" ]; then #1
-        echo -e "\nProceed with Core Update? [y/n]"
+        echo -e "\nProceed with Core Update? [y]"
         read answer
         echo -e "\n--------------"
         if [ "$answer" = "y" ]; then
@@ -55,28 +55,40 @@ function update_core () { #update wordpress, only when there is a new version
 
 function gitwp(){
     i=0
-    plugins=$(wp plugin list --update=available --field=name)
-    oldversions=$(wp plugin list --update=available --field=version)
-    for plugin in "${plugins[@]}"; do
+    cd wp-content  &>/dev/null
+    for plugin in $(wp plugin list --update=available --field=name); do
+        old_v=$(wp plugin get $plugin --field=version)
         echo "=============================="
         echo "Updating $plugin"
-        wp plugin update $plugin
+        sleep 1
+        #wp plugin update $plugin
         #new version
-        new_v=$(cat wp-content/plugins/$plugin/$plugin.php | grep -Po "(?<=Version: )([0-9]|\.)*(?=\s|$)")
+        #new_v=$(cat wp-content/plugins/$plugin/$plugin.php | grep -Po "(?<=Version: )([0-9]|\.)*(?=\s|$)")
+        new_v=$(wp plugin get $plugin --field=version)
+        echo "version: $new_v"
 
+        plugins[$i]="$plugin:$old_v --> $new_v"
         echo "------------------------------"
         echo "Writing Commit:"
-        echo "chore: update plugin $plugin:${oldversions[$i]} --> $version"
+        echo "chore: update plugin ${plugins[$i]}"
         echo "------------------------------"
-        git add -A wp-content/plugins/$plugin 
-        git commit -m "chore: update plugin $plugin:${oldversions[$i]} --> $version"
+        git add -A plugins/$plugin 
+        git commit -m "chore: update plugin ${plugins[$i]}"
+        echo "Push to Github? [y]"
+        read a
+        if [ "$a" = "y" ]; then
+            git push
+        else
+            "Not pushing"
+        fi
         ((i++)) #increment c-style
     done
+    cd -  &>/dev/null
     echo "=============================="
     echo "Summary:"
     echo "=============================="
     for p in "${!plugins[@]}"; do #get  index of array -> !
-        echo "${plugins[$p]} --> ${oldversions[$p]}"
+        echo "${plugins[$p]}"
     done
     sleep 2
 }
