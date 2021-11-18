2ef894638597a150abfaa014c2ba48c805648e13
Oswaldo Nickel <oswaldo.nickel@pfennigparade.de>
2021-11-18 11:50:58 +0100

    fix: comment

--- wpupdate.sh
+++ wpupdate.sh
@@ -40,7 +40,7 @@ done
 #${var##*SubStr} # drops substring from start of string up to last occurrence of `SubStr`
 #${var%SubStr*}  # drops substring from last occurrence of `SubStr` to end of string
 #${var%%SubStr*} # drops substring from first occurrence of `SubStr` to end of string
-process_dirs(){
+process_dirs(){ #split directories -> a,b,c sites[0]=a, sites[1]=b, sites[2]=c
     local dirs="$1"
     local site
     if [ ! -z "$dirs" ]; then #if something did go wrong
