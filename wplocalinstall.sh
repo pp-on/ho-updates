939b3996dd120b0dc9e6adbf86ab4f16d3ed88a8
Oswaldo Nickel <oswaldo.nickel@pfennigparade.de>
2022-03-09 15:58:19 +0100

    fix: EOF w/o indentation

--- wplocalinstall.sh
+++ wplocalinstall.sh
@@ -191,7 +191,7 @@ define( 'WP_DEBUG_DISPLAY', false );
 
 // Use dev versions of core JS and CSS files (only needed if you are modifying these core files)
 define( 'SCRIPT_DEBUG', true );
-    EOF
+EOF
 
      
 }
