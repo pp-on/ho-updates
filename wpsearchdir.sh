6db04bc35e3f6ce1ed9cf2dbf21bf49aedebd23d
Oswaldo Nickel <oswaldo.nickel@pfennigparade.de>
2022-03-17 11:34:41 +0100

    chore: wpsearchdir only does that from now on

--- wpsearchdir.sh
+++ wpsearchdir.sh
@@ -7,18 +7,8 @@
 #+----------------------------------+
 #|      Functions                   |
 #+----------------------------------+
-#| process_dirs()                   |
-#| split $argument in sites names   |
-#+----------------------------------+
 #| searchwp()                       |
 #| search for wp sites in $dir      |
-#+----------------------------------+
-#| list_wp_plugins()                |
-#+----------------------------------+
-#| print_sites()                    |
-#+----------------------------------+
-#| process_sites()                  |
-#| add or not a wpsite to an array  |
 #+----------------------------------+
 
 # Reset
@@ -40,20 +30,11 @@ total=0
 dir=./
 anzahl=0
 print=0
-sdirs="" #given dirs to be selected
 
 while [ $# -gt 0 ];do
     case $1 in
         -p)
             print=1
-            ;;
-        -b)
-            shift
-            sdirs="$1"
-            process_dirs "$sdirs"
-            ;;
-        -s)
-            searchwp
             ;;
         -v)
             verbose=1
@@ -82,97 +63,12 @@ done
 dirs=$(find "$dir"*/ -maxdepth 1 -mindepth 1 -type d)
 #check for wpsites and add them to array
 #for site in "${dirs[@]}"; do
-searchwp() {
-    local site #only valid within this function
-    for site in $(ls -d "$dir"*/); do
-        if [ -d "$site/wp-content/" ]; then
-            site=${site##"$dir"}
-            [ "$verbose" = "1" ] && sleep 1 && echo "Found $site"
-            sites+=("$site"); (( anzahl++ ))
-        fi
-    done
-
-}
-process_dirs(){ #split directories -> a,b,c sites[0]=a, sites[1]=b, sites[2]=c
-    local dirs="$1"
-    local site #only valid within this function
-    local site
-    if [ ! -z "$dirs" ]; then #if something did go wrong
-        while [ "$dirs" != "$site" ]; do
-            site=${dirs%%,*} #first element -> dirs=a,b,c site=a  
-            dirs=${dirs#"$site",} #new string w/o first element -> b,c
-            while [ ! -d "$dir$site" ]; do #it has to be a correct name
-                echo "$dir$site not found! Tipe in [n]ew name or [c]ontinue..."
-                read a
-                if [ "$a" = "n" ]; then
-                    echo "----------------"
-                    echo "enter new name: "
-                    read site
-                    echo "----------------"
-                elif [ "$a" = "c" ]; then
-                    site="" #first element is empty
-                    break #stop loop
-                else
-                    continue #startover
-                    #break
-                fi
-            done
-            [ ! -z "$site" ] && sites+=("$site") #copy into array
-            
-
-        done
+for site in $(ls -d "$dir"*/); do
+    if [ -d "$site/wp-content/" ]; then
+        site=${site##"$dir"}
+        [ "$verbose" = "1" ] && sleep 1 && echo "Found $site"
+        sites+=("$site"); (( anzahl++ ))
     fi
-}
-list_wp_plugins(){
-    local i
-    local a
-
-    for i in ${sites[@]}; do
-        echo -e "${Green}----------------"
-        td "$dir$i"  &>/dev/null #change to root wp of site
-        echo -e $i
-        echo -e "----------------${Color_Off}"
-        $wp plugin list --color
-        echo -e "${Yellow} $($wp plugin list --format=count) Plugins"
-        echo -e "${Purple}To continue press any key and enter...${Color_Off}"
-        read a
-        cd -  &>/dev/null #change back to orignal dir 
-    done
-}
-print_sites(){
-    local i
-    echo -e "${Yellow}----------------"
-    sleep 1
-    echo -e "${#sites[@]} selected websites" 
-    echo "----------------"
-    for i in ${sites[@]}; do
-        echo -e ${Cyan}$i
-    done
-    echo -e "${Yellow}----------------"
-}
-# w/o arguments
-[ "$print" = "1" ] && print_sites
-
-# -t -> how many found 
-[ "$total" = "1" ] && echo -e "\n=======\nTotal $anzahl WP-Sites"
-
-process_sites(){
-    local site #only valid within this function
-    if [ -z "$sites" ]; then #if no folders aka Websites were pass as argument
-        for site in $(ls -d $dir*/); do
-            if [ -d "$site/wp-content/" ]; then
-                seite=${site##"$dir"}
-                echo "Found $seite"
-                echo "Should it be processed? [y] "
-                read answer
-                echo -e "\n--------------"
-                if [ "$answer" = "y" ]; then
-                    #only names
-                    site=${site#"$dir"}
-                    site=${site%%/}
-                    sites+=("$site")
-                fi
-            fi
-        done
-    fi
-}
+done
+[ -n "$total" ] && echo -e "${Yellow}${anzahl}${Color_Off} WordPress Sites
+found"
