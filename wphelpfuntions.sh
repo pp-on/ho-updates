aa8e930573e3287372f32baa98b2f903f47c4719
Oswaldo Nickel <oswaldo.nickel@pfennigparade.de>
2022-03-30 15:35:09 +0200

    fix: colors

--- wphelpfuntions.sh
+++ wphelpfuntions.sh
@@ -27,66 +27,19 @@
 #| print text with line "-" or "="  |
 #+----------------------------------+
 
-
-
-wp="wp"         #where is wp-cli 
-verbose=0
-total=0
-dir=./
-anzahl=0
-print=0
-sdirs="" #given dirs to be selected
-
-while [ $# -gt 0 ];do
-    case $1 in
-        -p)
-            print=1
-            ;;
-        -b)
-            shift
-            sdirs="$1"
-            process_dirs "$sdirs"
-            ;;
-        -v)
-            verbose=1
-            ;;
-        -t)
-            total=1
-            ;;
-        -w)
-            shift
-            wp=${1}
-            ;;
-        -d)
-            shift
-            dir=$1
-            ;;
-        -h)
-            echo "wpsearchdir.sh [-w path/to/wpcli][-s][-p][-v][-t][-d TARGET DIR][-b (dir1,dir2,...)]"
-            exit
-            ;;
-    esac
-    #next argument -> e.g. $2 becomes $1, $3 becomes $2...
-    shift
-done
-#for dirs
-#dirs=($(ls -lh -d */ | awk '{print $9}' | sed 's/\///g'))
-dirs=$(find "$dir"*/ -maxdepth 1 -mindepth 1 -type d)
-#check for wpsites and add them to array
-#for site in "${dirs[@]}"; do
 colors(){
-# Reset
-Color_Off="\e[0m"       # Text Reset
+    # Reset
+    Color_Off="\e[0m"       # Text Reset
 
-# Regular Colors
-Black="\e[30m"        # Black
-Red="\e[31m"          # Red
-Green="\e[32m"        # Green
-Yellow="\e[33m"       # Yellow
-Blue="\e[34m"         # Blue
-Purple="\e[35m"       # Purple
-Cyan="\e[36m"         # Cyan
-White="\e[37m"        # White
+    # Regular Colors
+    Black="\e[30m"        # Black
+    Red="\e[31m"          # Red
+    Green="\e[32m"        # Green
+    Yellow="\e[33m"       # Yellow
+    Blue="\e[34m"         # Blue
+    Purple="\e[35m"       # Purple
+    Cyan="\e[36m"         # Cyan
+    White="\e[37m"        # White
 
 }
 searchwp() {
