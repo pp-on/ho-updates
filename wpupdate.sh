3947c3004b796874defd3d6b2267d214bd70e693
Oswaldo Nickel <oswaldo.nickel@pfennigparade.de>
2022-07-19 14:24:34 +0200

    fix: show plugins

--- wpupdate.sh
+++ wpupdate.sh
@@ -141,6 +141,7 @@ for site in "${sites[@]}"; do
 
    #upd_avail=$($wp core check-update 2>/dev/null| grep Success) #0 -> ok ,1 -> err in bash
    plugins_up=$($wp plugin list --update=available) 
+    $wp plugin list --update=available
    if [ -z "$plugins_up" ]; then
        echo "Nothing to be updated!"
    else
