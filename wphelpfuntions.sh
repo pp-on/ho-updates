c7d4b208f6b7eecd268211d78d018dc0056c400c
Nickel Oswaldo <oswaldo.nickel@pfennigparade.de>
2025-03-14 13:30:22 +0100

    add getCPT()

--- wphelpfuntions.sh
+++ wphelpfuntions.sh
@@ -549,3 +549,44 @@ wp_block_se (){
         $wp option update blog_public 0
     #done
 }
+
+wp_getCPT(){
+
+    # Standardwerte setzen
+    CPTS=""
+
+    # Argumente parsen
+    while getopts "c:" opt; do
+      case $opt in
+        c) CPTS="$OPTARG" ;;
+        *) echo "Ungültige Option" >&2; exit 1 ;;
+      esac
+    done
+
+    # Prüfen, ob CPTs angegeben wurden
+    if [[ -z "$CPTS" ]]; then
+      echo "Fehlende CPTs! Nutzung: $0 -c cpt1,cpt2,..."
+      exit 1
+    fi
+
+    # SQL WHERE-Bedingung für die CPTs generieren
+    CPTS_SQL=$(echo "$CPTS" | sed "s/,/','/g")
+    CPTS_SQL="('$CPTS_SQL')"
+
+    # Export CPTs
+    wp db query "SELECT * FROM wp_posts WHERE post_type IN $CPTS_SQL" --allow-root > cpts.sql
+
+    # Export Postmeta (benutzerdefinierte Felder)
+    wp db query "SELECT * FROM wp_postmeta WHERE post_id IN (SELECT ID FROM wp_posts WHERE post_type IN $CPTS_SQL)" --allow-root >> cpts.sql
+
+    # Export Taxonomien (Beziehungen zwischen CPTs und Taxonomien)
+    wp db query "SELECT * FROM wp_term_relationships WHERE object_id IN (SELECT ID FROM wp_posts WHERE post_type IN $CPTS_SQL)" --allow-root >> cpts.sql
+
+    # Export Taxonomie-Informationen
+    wp db query "SELECT * FROM wp_term_taxonomy WHERE term_taxonomy_id IN (SELECT term_taxonomy_id FROM wp_term_relationships WHERE object_id IN (SELECT ID FROM wp_posts WHERE post_type IN $CPTS_SQL))" --allow-root >> cpts.sql
+
+    # Export Begriffe (Terms)
+    wp db query "SELECT * FROM wp_terms WHERE term_id IN (SELECT term_id FROM wp_term_taxonomy WHERE term_taxonomy_id IN (SELECT term_taxonomy_id FROM wp_term_relationships WHERE object_id IN (SELECT ID FROM wp_posts WHERE post_type IN $CPTS_SQL))))" --allow-root >> cpts.sql
+
+    echo "Expo          rt abgeschlossen: cpts.sql"
+}
